769f22c1dce18aaf9aa180ef87dacb4c1aabfad3
Vuyo Mtotywa <mihlemtotywa@gmail.com>
2026-02-04 14:24:37 +0200

    Add files via upload

---
+++ 3.SalaryCalculator/SalaryCalculator2009/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Intsentiation
+        Dim objSalraryCalculator As New ClsSalaryCalculator
+
+        'Initialiasation
+        objSalraryCalculator._decSalary = CDec(txtSalary.Text)
+        objSalraryCalculator._strWorkerRank = txtRank.Text
+        objSalraryCalculator._decIncresePercentage = CDec(txtSalaryIncrease.Text)
+
+        'Envoke/Call the function
+        lblAnswer.Text = objSalraryCalculator.CalcSalary().ToString()
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
